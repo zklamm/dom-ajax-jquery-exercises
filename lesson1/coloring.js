521375d55f2a6b9612a49c212752e6c11df95b4c
Zac Klammer <zklamm@gmail.com>
2019-09-13 08:29:40 -0500

    Update notes

---
+++ lesson1/coloring.js
@@ -0,0 +1,4 @@
+function colorGeneration(generation) {
+  // collect all elements in generation
+  // add .generation-color class to each of these elements
+}
--- lesson1/sliceTree.js
+++ lesson1/sliceTree.js
@@ -1,8 +1,26 @@
-function sliceTree(start, stop) {
-  //does the element at start id have children?
-    // yes, then for each child:
-      // if its id === stop, add this and all parent elems to start id to result
-      // else see if this element has children
-    // no, then return;
-  // if result empty, return undefined, else, return result
+function sliceTree(start, end) {
+  var slice = [];
+
+  function populateSlice(child) {
+    slice.unshift(child.tagName);
+
+    while (child.id !== String(start)) {
+      child = child.parentNode;
+      slice.unshift(child.tagName);
+    }
+  }
+
+  (function search(id) {
+    var element = document.getElementById(id);
+    var children = element.children;
+
+    if (children.length > 0) {
+      for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        child.id === String(end) ? populateSlice(child) : search(child.id);
+      }
+    }
+  })(start);
+
+  return slice.length > 0 ? slice : undefined;
 }
