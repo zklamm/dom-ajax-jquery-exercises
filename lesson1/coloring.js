000a1231232e250dcb8f86ab5f76fe05e6fd4a2b
Zac Klammer <zack@baytekinternational.com>
2019-09-13 16:49:35 -0500

    Update notes

--- lesson1/coloring.js
+++ lesson1/coloring.js
@@ -1,4 +1,27 @@
-function colorGeneration(generation) {
-  // collect all elements in generation
-  // add .generation-color class to each of these elements
+function colorGeneration(targetGeneration) {
+  var generation = 0;
+  var parents = [document.body];
+  var elements;
+
+  while (generation < targetGeneration) {
+    generation += 1;
+    elements = getAllChildrenOf(parents);
+    parents = elements;
+  }
+
+  if (elements) color(elements);
+}
+
+function color(elements) {
+  elements.forEach(function(element) {
+    element.classList.add('generation-color');
+  });
+}
+
+function getAllChildrenOf(parents) {
+  return parents.map(function(element) {
+    return [].slice.call(element.children);
+  }).reduce(function(collection, children) {
+    return collection.concat(children);
+  }, []);
 }
