6d07ae6e42c6d71f8aadc98521af33ac59d19f06
Zac Klammer <zklamm@gmail.com>
2019-10-09 05:46:35 -0500

    update notes

--- lesson3/booking_app_node/public/test.js
+++ lesson3/booking_app_node/public/test.js
@@ -6,16 +6,36 @@ function retrieveSchedules() {
   request.responseType = 'json';
 
   request.addEventListener('load', function(e) {
+    var schedules = request.response;
+    var staffs = [];
+    var tally = [];
 
+    if (schedules.length > 0) {
+      schedules.forEach(function(schedule) {
+        var key = "staff" + String(schedule.staff_id);
+        if (staffs.indexOf(key) === -1) {
+          staffs.push(key);
+          tally.push(1);
+        } else {
+          tally[staffs.indexOf(key)] += 1;
+        }
+      });
+
+      alert(tally.map(function(_, index) {
+        return staffs[index] + ": " + tally[index];
+      }).join("\n"));
+    } else {
+      alert('There are currently no schedules available for booking.');
+    }
   });
 
   request.addEventListener('timeout', function(e) {
-
+    alert('It is taking longer than usual. Please try again later.')
   });
 
-  request.addEventListener('loadend', function(e) {
-
+  request.addEventListener('loadend', function() {
+    alert('The request has completed.')
   });
 
-  request.send():
+  request.send();
 }
