626e34104b8d55b69f62ad0db340e771bcfd0cfa
Zac Klammer <zack@baytekinternational.com>
2019-11-20 16:47:04 -0600

    update

---
+++ ES6Practice/practice.js
@@ -0,0 +1,5 @@
+const isLeapYear = year => year % 100 !== 0 || year % 400 === 0 && year % 4 === 0;
+
+// function isLeapYear(year) {
+//   return year % 100 !== 0 || year % 400 === 0 && year % 4 === 0;
+// }
