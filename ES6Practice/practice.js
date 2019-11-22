583b7d871d0dc4916cca652146e9c1cb02466149
Zac Klammer <zack@baytekinternational.com>
2019-11-22 14:42:35 -0600

    update

--- ES6Practice/practice.js
+++ ES6Practice/practice.js
@@ -1,5 +1,31 @@
-const isLeapYear = year => year % 100 !== 0 || year % 400 === 0 && year % 4 === 0;
+// input: ary
+// output: new ary where first element of input ary is now at the end
+// assume: return undefined if input is not ary
+//         return [] if input is []
+// logic:
+//   use guard clauses for assumptions
+//   first = ary[0]
+//   rest = ary.slice(1)
+//   return rest.push(first);
 
-// function isLeapYear(year) {
-//   return year % 100 !== 0 || year % 400 === 0 && year % 4 === 0;
+function rotateArray(ary) {
+  // debugger;
+  if (!Array.isArray(ary)) return;
+  if (ary.length === 0) return [];
+
+  const first = ary[0];
+  const rest = ary.slice(1);
+  return rest.push(first);
+}
+
+// function rotateArray(array) {
+//   if (!Array.isArray(array)) {
+//     return;
+//   }
+
+//   if (array.length === 0) {
+//     return [];
+//   }
+
+//   return array.slice(1).concat(array[0]);
 // }
