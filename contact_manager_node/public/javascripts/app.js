c66eb1897313536f97ae289bfe9bb889377f5df1
Zac Klammer <zklamm@gmail.com>
2020-01-31 11:30:05 -0600

    update

--- contact_manager_node/public/javascripts/app.js
+++ contact_manager_node/public/javascripts/app.js
@@ -1,26 +1,2 @@
 $(() => {
-  const contactsTemplate = Handlebars.compile($('#contacts').html());
-  const $contactsList = $('#contactsList');
-  const $createContact = $('#createContact');
-  const $featureBar = $('#featureBar');
-
-  $contactsList.append(contactsTemplate());
-
-  contactsTemplate();
-
-  $('.addContact').on('click', e => {
-    e.preventDefault();
-
-    $createContact.fadeIn();
-    $contactsList.fadeOut();
-    $featureBar.fadeOut();
-
-    $('.cancel').on('click', e => {
-      e.preventDefault();
-
-      $createContact.fadeOut();
-      $contactsList.fadeIn();
-      $featureBar.fadeIn();
-    });
-  });
 });
