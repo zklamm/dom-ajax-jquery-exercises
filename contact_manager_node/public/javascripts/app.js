97b8d48a69d03415548f172b983d19049ec18bee
Zac Klammer <zklamm@gmail.com>
2020-02-03 08:05:46 -0600

    update

--- contact_manager_node/public/javascripts/app.js
+++ contact_manager_node/public/javascripts/app.js
@@ -1,2 +1,46 @@
 $(() => {
+  const templates = {};
+  let contacts;
+
+  $('script[type="text/x-handlebars"]').each(function() {
+    const $script = $(this).remove();
+    templates[$script.attr('data-template')] = Handlebars.compile($script.html());
+  });
+
+  // debugger
+
+  const app = {
+    toggleForm(e) {
+      e.preventDefault();
+
+      $('#featureBar').slideToggle();
+      $('#contactsList').slideToggle();
+      $('#emptyContacts').slideToggle();
+      $('#createContact').slideToggle({
+        start() {
+          $(this).css('display', 'inline-block');
+        }
+      });
+    },
+
+    bind() {
+      $('.add, .cancel').on('click', this.toggleForm.bind(this));
+    },
+
+    init() {
+      this.bind();
+    },
+  };
+
+  $.ajax({
+    url: 'api/contacts'
+  }).done(json => {
+    contacts = json;
+    renderContacts();
+    app.init();
+  });
+
+  function renderContacts() {
+    $('#contactsList').html(templates.contacts());
+  }
 });
