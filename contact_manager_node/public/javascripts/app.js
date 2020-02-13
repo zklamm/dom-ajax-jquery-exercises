a81e78230d969efead64f36f3733cf53753f0b01
Zac Klammer <zklamm@gmail.com>
2020-02-13 11:47:21 -0600

    update

--- contact_manager_node/public/javascripts/app.js
+++ contact_manager_node/public/javascripts/app.js
@@ -1,6 +1,5 @@
 $(() => {
   const templates = {};
-  let contacts;
 
   $('script[type="text/x-handlebars"]').each(function() {
     const $script = $(this).remove();
@@ -10,13 +9,17 @@ $(() => {
   Handlebars.registerPartial('contact', templates.contact);
   Handlebars.registerPartial('noContacts', templates.noContacts);
 
+  $('#createContact').html(templates.createContact());
 
   const app = {
+    $requiredFields: $('#fullName, #email, #phone'),
+    form: document.querySelector('form'),
+
     toggleForm(e) {
       e.preventDefault();
-
-      $('#featureBar').slideToggle();
-      $('#contactsList').slideToggle();
+      this.removeErrors();
+      this.form.reset();
+      $('#featureBar, #contactsList').slideToggle();
       $('#createContact').slideToggle({
         start() {
           $(this).css('display', 'inline-block');
@@ -24,38 +27,91 @@ $(() => {
       });
     },
 
-    submit(e) {
-      e.preventDefault();
+    deleteContact(e) {
+      if (confirm('Do you want to delete the contact ?')) {
+        const id = $(e.target).closest('li').attr('data-id');
+        $.ajax({
+          url: `api/contacts/${id}`,
+          method: 'DELETE',
+        }).done(this.getContacts());
+      }
+    },
 
+    editContact(e) {
+      const id = $(e.target).closest('li').attr('data-id');
+
+      this.toggleForm(e);
+
+      $.ajax({
+        url: `api/contacts/${id}`
+      }).done(json => {
+        // debugger
+        $('#createContact').html(templates.createContact({json}))
+      });
+    },
+
+    getContacts() {
+      $.ajax({
+        url: 'api/contacts'
+      }).done(json => {
+        $('#contactsList').html(templates.contacts({contacts: json}));
+        $('.edit').on('click', this.editContact.bind(this));
+        $('.delete').on('click', this.deleteContact.bind(this));
+      });
+    },
+
+    createContact() {
       $.ajax({
         url: 'api/contacts',
         dataType: 'json',
         method: 'POST',
         data: $('form').serialize(),
-      }).done(json => {
-        console.log(json);
+      }).done(this.getContacts());
+    },
+
+    removeErrors($input) {
+      $input = $input || this.$requiredFields;
+      $input.removeClass('error');
+      $input.prev('label').removeClass('error');
+      $input.next('small').removeClass('error');
+    },
+
+    showErrors() {
+      const self = this;
+      this.$requiredFields.each(function() {
+        const $input = $(this);
+        if (!this.checkValidity()) {
+          $input.addClass('error');
+          $input.prev('label').addClass('error');
+          $input.next('small').addClass('error');
+        } else {
+          self.removeErrors($input);
+        }
       });
     },
 
+    submit(e) {
+      if (this.form.checkValidity()) {
+        e.preventDefault();
+        this.removeErrors();
+        this.createContact();
+        this.toggleForm(e);
+        this.form.reset();
+      } else {
+        this.showErrors();
+      }
+    },
+
     bind() {
-      $('.add, .cancel, .submit').on('click', this.toggleForm.bind(this));
       $('.submit').on('click', this.submit.bind(this));
+      $('.add, .cancel').on('click', this.toggleForm.bind(this));
     },
 
     init() {
+      this.getContacts();
       this.bind();
     },
   };
 
-  function renderContacts() {
-    $('#contactsList').html(templates.contacts({contacts: contacts}));
-  }
-
-  $.ajax({
-    url: 'api/contacts'
-  }).done(json => {
-    contacts = json;
-    renderContacts();
-    app.init();
-  });
+  app.init();
 });
