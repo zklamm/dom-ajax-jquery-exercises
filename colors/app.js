3c9c0ef6307f93183ebb621eb829882044020c45
Zac Klammer <zack@baytekinternational.com>
2020-02-13 16:52:25 -0600

    update

--- colors/app.js
+++ colors/app.js
@@ -10,98 +10,44 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// function getColors() {
-//   const colors = [];
-
-//   for (let i = 15; i >= 1; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`f${h}${h}`);
-//   }
-//   for (let i = 15; i >= 0; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`${h}00`);
-//   }
-
-//   for (let i = 15; i >= 1; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`ff${h}`);
-//   }
-//   for (let i = 15; i >= 0; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`${h}${h}0`);
-//   }
-
-//   for (let i = 15; i >= 1; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`${h}f${h}`);
-//   }
-//   for (let i = 15; i >= 0; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`0${h}0`);
-//   }
-
-//   for (let i = 15; i >= 1; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`${h}ff`);
-//   }
-//   for (let i = 15; i >= 0; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`0${h}${h}`);
-//   }
-
-//   for (let i = 15; i >= 1; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`${h}${h}f`);
-//   }
-//   for (let i = 15; i >= 0; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`00${h}`);
-//   }
-
-//   for (let i = 15; i >= 1; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`f${h}f`);
-//   }
-//   for (let i = 15; i >= 0; i -= 1) {
-//     const h = i.toString(16);
-//     colors.push(`${h}0${h}`);
-//   }
-
-//   return colors;
-// }
-
 function getReds() {
-  const shades = [];
-  const white = [255, 255, 255];
-  let [ r, g, b ] = white;
+  const lights = colorize([255, 255, 255]);
+  const peak = lights[lights.length - 1];
+  const darks = blacken(peak);
+  return toHex(lights.concat(darks));
+}
 
-  do {
-    let shade = decsToHex([r, g, b]);
-    shades.push(shade);
+function toHex(shades) {
+  return shades.map(shade => {
+    return shade.map(component => {
+      return component.toString(16).padStart(2, '0');
+    }).join('');
+  });
+}
+
+function colorize([ r, g, b ]) {
+  const shades = [];
+
+  while (r > 0 && g > 0 && b > 0) {
+    shades.push([r, g, b]);
     r -= 0;
     g -= 17;
     b -= 17;
-  } while (r > 0 && g > 0 && b > 0);
+  }
 
-  do {
-    let shade = decsToHex([r, g, b]);
-    shades.push(shade);
-    r -= 17;
-    g -= 0;
-    b -= 0;
-  } while (r > 0 || g > 0 || b > 0);
-
-  // while (r > 0 || g > 0 || b > 0) {
-  //   let shade = decsToHex([r, g, b]);
-  //   shades.push(shade);
-  //   r -= 17;
-  //   g -= 0;
-  //   b -= 0;
-  // }
-// debugger
+  shades.push([r, g, b]);
   return shades;
 }
 
-function decsToHex(decs) {
-  return decs.map(dec => dec.toString(16).padStart(2, '0')).join('');
+function blacken([ r, g, b ]) {
+  const shades = [];
+
+  while (r > 0 || g > 0 || b > 0) {
+    r -= 17;
+    g -= 0;
+    b -= 0;
+    shades.push([r, g, b]);
+  }
+
+  return shades;
 }
